192a8f14f57d168ce41d77ef3358438611d7907d
EUNJEONGMUN <munej26@gmail.com>
2022-04-02 12:39:09 +0900

    Add gitignore

--- src/main/java/com/example/demo/config/secret/MessageKey.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.config.secret;
-
-public class MessageKey {
-    public static String API_KEY = "NCSHOFJGVNLKXCGM";
-    public static String API_SECRET ="GUSZWABZJCDLC9QLKZQ6R5NDN0BRNXL3";
-    public static String PHONE_NUMBER = "010-4337-4232";
-}
