91db6bdd7f30f0c64b58a9a8a86384b472ef8c46
EUNJEONGMUN <munej26@gmail.com>
2022-04-08 01:09:04 +0900

    [ADD] DAO Interface

---
+++ src/main/java/com/example/demo/src/orders/OrderDaoInterface.java
@@ -0,0 +1,7 @@
+package com.example.demo.src.orders;
+
+import com.example.demo.src.orders.model.Req.PostCreateCartReq;
+
+public interface OrderDaoInterface {
+
+}
---
+++ src/main/java/com/example/demo/src/store/StoreDaoInterface.java
@@ -0,0 +1,4 @@
+package com.example.demo.src.store;
+
+public interface StoreDaoInterface {
+}
